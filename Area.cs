a6ddb0c97afdf90ae13a6ecf00187fbbaa009e7b
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-08-25 12:33:17 -0300

    Add files via upload

---
+++ Area.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado t;
+            t = new Quadrado();
+
+
+            Console.WriteLine("Digite o valor da base");
+            t.setl(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite o valor da altura");
+            t.setl(double.Parse(Console.ReadLine()));
+            t.calcular();
+            Console.WriteLine("O valor da aresta é {0} da altura {1} área é de {2}", t.getl(), t.getl(), t.geta());
+
+
+
+
+        }
+    }
+}
